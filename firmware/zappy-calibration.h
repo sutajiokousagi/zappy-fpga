98b92d6555c4de93fe1fad3eb60a417d685cafe1
bunnie <bunnie@kosagi.com>
2020-06-28 04:44:28 -0400

    update logo, prepare calibration headers and serialization for unit #2

--- firmware/zappy-calibration.h
+++ firmware/zappy-calibration.h
@@ -75,4 +75,54 @@ fast: 0.16229V
 #endif
 
 
+#if ZAPPY_SERIAL == 2
+/*
+Calibration curve measurements (serial #1): 
+
+At temperature: 29.5C
+
+0.0027V
+slow: 0.715mV
+fast: 0.957mV
+
+4.9967 V
+slow: 21.77mV
+fast: 23.40mV
+
+9.9903 V
+slow: 43.49mV
+fast: 46.52mV
+
+14.986 V
+slow: 6.22 mV  (+/-0.01mV)
+fast: 69.67 mV  (+/-0.01mV)
+
+19.980 V
+slow: 86.94mV
+fast: 92.83mV
+
+24.977 V
+slow: 0.10866V
+fast: 0.11597V
+
+34.966 V
+slow: 0.15212V
+fast: 0.16229V
+ */
+#define HOSTNAME  "zappy-02 a"
+#define FAST_M  230.156015 // 215.7720466   // resistor updated to 11.335k for exact gain matching on fast/slow paths
+#define FAST_B  0.176922133 //-0.0488699
+#define SLOW_M  229.9235716
+#define SLOW_B  -0.008779325
+#define P5V_ADC 5.009
+#define P5V_ADC_LOGIC 5.000  // not yet measured
+#define FS_DAC  9.9643
+#define HVDAC_M 6556.4623
+#define HVDAC_B 201.1304
+#define CAPRES  46.79   // ohms, measured at 27.5C
+#define ENERGY_COEFF 0.000000001691356 // joules per LSB of accumulated energy
+#define ONEJOULE 591241583.67  // one joule of accumulated energy in LSBs
+#endif
+
+
 #endif
